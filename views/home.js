2457f40f3d6e065ccd38ce9b3435505db9b8363c
Vincent Orback <vorback@gmail.com>
2019-03-08 16:38:27 +0100

    add featured posts and events to home view

--- views/home.js
+++ views/home.js
@@ -1,5 +1,9 @@
 var html = require('choo/html')
 var view = require('../components/view')
+var hero = require('../components/hero')
+var grid = require('../components/grid')
+var card = require('../components/card')
+var event = require('../components/event')
 var asElement = require('prismic-element')
 var serialize = require('../components/text/serialize')
 var { asText, resolve } = require('../components/base')
@@ -13,9 +17,53 @@ function home (state, emit) {
         if (err) throw err
         if (!doc) return html`<div></div>`
 
+        var featuredPosts = newsData.map(function (data) {
+          return card(data)
+        })
+
+        var featuredEvents = eventData.map(function (data) {
+          return event(data)
+        })
+
+        /*
+        var featuredPosts = doc.data.featured_posts.map(function (data) {
+          return card(data)
+        })
+
+        var featuredEvents = doc.data.featured_events.map(function (data) {
+          return event(data)
+        })
+        */
+
         return html`
-          <div class="u-container">
-            ${doc.data.title ? asElement(doc.data.title, resolve, serialize) : null}
+          <div>
+            ${state.cache(hero, `hero-${doc.id}`).render()}
+
+            <div class="View-section">
+              <div class="u-container">
+                ${grid({
+                  size: {
+                    md: '1of2',
+                    lg: '1of3'
+                  }
+                }, featuredPosts)}
+              </div>
+            </div>
+
+            <div class="View-section">
+              <div class="u-container">
+                ${grid({
+                  size: {
+                    md: '1of2'
+                  }
+                }, featuredEvents)}
+              </div>
+            </div>
+
+            <div class="View-section View-section--grayToWhite">
+              <p class="u-textCenter">karta</p>
+            </div>
+
           </div>
         `
       })}
@@ -44,3 +92,81 @@ function meta (state) {
     return props
   })
 }
+
+
+
+/* TODO: Remove dummy data */
+
+var newsData = [
+  {
+    date: {
+      text: '10-11 April',
+      datetime: ''
+    },
+    title: 'Grow 2019 agri summit',
+    body: '',
+    location: 'Christchurch, New Zealand',
+    link: {
+      href: '#/'
+    },
+    image: {
+      src: 'https://placehold.it/800x400'
+    }
+  },
+  {
+    title: 'Video title',
+    body: 'Common outcome—to shift perspective, to learn, and to evolve.',
+    link: {
+      href: '#/',
+      title: 'Watch video'
+    },
+    image: {
+      src: 'https://placehold.it/400x800'
+    },
+    video: true
+  },
+  {
+    date: {
+      text: '25 April',
+      datetime: ''
+    },
+    title: 'Salim Ismail Exponential Impact',
+    body: '',
+    location: 'São Paulo, Brazil',
+    link: {
+      href: '#/'
+    },
+    image: {
+      src: 'https://placehold.it/400x800'
+    }
+  },
+  {
+    title: 'Forget preparing for the future, we need to create it',
+    body: 'How new technologies are helping organic farms increase their margins—and propelling sustainability into the mainstream.',
+    link: {
+      href: '#/'
+    },
+    image: {
+      src: 'https://placehold.it/400x800'
+    },
+    community: false
+  }
+]
+
+var eventData = [
+  {
+    date: {
+      text: '10-11 April',
+      datetime: ''
+    },
+    title: 'Grow 2019 agri summit',
+    body: '',
+    location: 'Christchurch, New Zealand',
+    link: {
+      href: '#/'
+    },
+    image: {
+      src: 'https://placehold.it/800x400'
+    }
+  }
+]
